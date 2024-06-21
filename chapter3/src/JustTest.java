b5c9917c9b955aba3ad4733b5f195baa1d017ba6
thefighter01 <mahmoudbubg1234@gmail.com>
2024-06-21 08:20:50 +0300

    commiting

--- chapter3/src/JustTest.java
+++ chapter3/src/JustTest.java
@@ -6,8 +6,8 @@ public class JustTest {
         byte b = 50;
         byte c = 100;
         int d = a * b / c; // java promotes (short , char , byte ) to int when we have an expression
-        // if i tried to put (a * b) / c into byte it will give me compile-time-error cause we can not convert int into byte by explicit way
-        // we must do it implicit
+        // if i tried to put (a * b) / c into byte it will give me compile-time-error cause we can not convert int into byte in implicit way
+        // we must do it explicit
 
         //byte d = (a * b) / c;
         System.out.println(d);
