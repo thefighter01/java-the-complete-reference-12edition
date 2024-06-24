cc2665dfedf8c433e8b30c27ebc0d3c63a4e11cf
thefighter01 <mahmoudbubg1234@gmail.com>
2024-06-25 02:20:58 +0300

    theEndOfChapter04

---
+++ chapter04/src/Ternary.java
@@ -0,0 +1,8 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int i , k;
+        i = 10;
+        k = (i < 0 ? -i : i);
+        System.out.println("the absolute value of " + i + " is " + k);
+    }
+}
